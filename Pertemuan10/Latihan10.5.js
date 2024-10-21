4824bbb9c64fa2b7bafb962f4e880ec77c1e6dfc
MuhammadFahryMaulana <fhymaulana@gmail.com>
2024-10-21 13:08:25 +0700

    Add files via upload

---
+++ Pertemuan10/Latihan10.5.js
@@ -0,0 +1,9 @@
+// Latihan10.5.js
+function showMessage() {
+    alert("Halo! Anda sedang belajar JavaScript di Duniailkom.");
+}
+
+// Memanggil fungsi saat halaman dimuat
+window.onload = function () {
+    showMessage();
+};
